138902e34a4136038658030118e387e3f65583b8
Yanisefrei <yanis.aissani@efrei.net>
2025-01-09 15:44:25 +0100

    dependencies: express et nodemon

--- Node.js
+++ Node.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Bienvenue sur notre API REST simple!');
+});
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
